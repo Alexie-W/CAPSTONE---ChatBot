749580a4b48cd43816cb52cf5c50bf154a0e5a46
Alexie-Campbell <alexie.campbell@mymona.uwi.edu>
2024-07-26 15:58:15 -0500

    conv app.py updated

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-
 from app import create_app, db
 from app.models import Conversation
 import requests
@@ -15,15 +14,15 @@ def home():
 @app.route('/process', methods = ['POST'])
 def process_input():
     user_input = request.json.get('input')    
-    rasa_response = requests.post('http://localhost:5005/webhooks/rest/webhook', json={"message": user_input})    
-    response_data = rasa_response.json()
-    response_text = response_data[0]['text'] if response_data else "Sorry, I didn't get that."
+    mm_response = requests.post('http://localhost:5005/webhooks/rest/webhook', json={"message": user_input})    
+    response_data = mm_response.json()
+    response = response_data[0]['text'] if response_data else "Sorry, I did not understand."
     
 
-    conversation = Conversation(user_input=user_input, response_text=response_text)
+    conversation = Conversation(user_input=user_input, response=response)
     db.session.add(conversation)
     db.session.commit()
-    return jsonify({'response': response_text})
+    return jsonify({'response': response})
 
 
 if __name__ == '__main__':
